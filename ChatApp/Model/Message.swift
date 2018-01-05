22ab8ed4f52a323b377ce758dc7fe95b636c496b
Dan <dosullivan557@gmail.com>
2018-01-05 18:03:55 +0000

    Added a model for messages

---
+++ ChatApp/Model/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  ChatApp
+//
+//  Created by Danny on 05/01/2018.
+//  Copyright © 2018 Danny. All rights reserved.
+//
+
+import UIKit
+
+class Message: NSObject {
+    var sendId: String?
+    var receiveId: String?
+    var message: String?
+    var timestamp: String?
+}
