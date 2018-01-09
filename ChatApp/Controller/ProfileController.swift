330e41ae99bb804e2cf1afb7e889a196a5c81108
Dan <dosullivan557@gmail.com>
2018-01-09 23:03:10 +0000

    added name view

--- ChatApp/Controller/ProfileController.swift
+++ ChatApp/Controller/ProfileController.swift
@@ -1,3 +1,11 @@
+//
+//  ProfileController.swift
+//  ChatApp
+//
+//  Created by Danny on 09/01/2018.
+//  Copyright © 2018 Danny. All rights reserved.
+//
+
 import UIKit
 import Firebase
 class ProfileController : UIViewController {
@@ -10,9 +18,21 @@ class ProfileController : UIViewController {
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
+    let nameLabel : UITextView = {
+       let name = UITextView()
+        name.allowsEditingTextAttributes = false
+        name.isEditable = false
+        name.isUserInteractionEnabled = false
+        name.translatesAutoresizingMaskIntoConstraints = false
+        name.textAlignment = .center
+        name.font = UIFont(name: "arial", size: 20)
+        return name
+    }()
+    
     override func viewDidLoad() {
         view.backgroundColor = UIColor.white
         view.addSubview(profileImage)
+        view.addSubview(nameLabel)
         setupFields()
     func setupFields(){
         profileImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
@@ -20,4 +40,9 @@ class ProfileController : UIViewController {
         profileImage.widthAnchor.constraint(equalToConstant: 150).isActive = true
         profileImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
         
+        nameLabel.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 20).isActive = true
+        nameLabel.centerXAnchor.constraint(equalTo:profileImage.centerXAnchor).isActive = true
+        nameLabel.widthAnchor.constraint(equalTo: profileImage.widthAnchor).isActive = true
+        nameLabel.heightAnchor.constraint(equalToConstant: 150).isActive = true
+    }
     }
