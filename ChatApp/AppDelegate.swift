e53f16a8bf6e62d6b07a0b11d74664f4f32f9414
Dan <dosullivan557@gmail.com>
2018-01-09 23:01:41 +0000

    Changed view to load the tab view as well

--- ChatApp/AppDelegate.swift
+++ ChatApp/AppDelegate.swift
@@ -24,8 +24,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         
-        window?.rootViewController = UINavigationController(rootViewController: MessagesController())
-        
+//        window?.rootViewController = UINavigationController(rootViewController: MessagesController())
+        window?.rootViewController = MyTabBar()
+
+
         return true
     }
 
