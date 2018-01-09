965b708f69c1dd4b78ec37478d0db66b119f7c65
Dan <dosullivan557@gmail.com>
2018-01-09 22:58:48 +0000

    Created my own tab bar

---
+++ ChatApp/View/MyTabBar.swift
@@ -0,0 +1,30 @@
+//
+//  MyTabBar.swift
+//  ChatApp
+//
+//  Created by Danny on 09/01/2018.
+//  Copyright © 2018 Danny. All rights reserved.
+//
+
+import UIKit
+
+class MyTabBar: UITabBarController {
+    var tabList = [UIViewController]()
+    override func viewDidLoad() {
+
+        createElements(view: MessagesController(), title: "Messages", imageName: "Messages")
+        createElements(view: ProfileController(), title: "Profile", imageName: "profile")
+
+        
+        viewControllers = tabList
+    }
+    func createElements(view: UIViewController,title: String, imageName: String) -> UINavigationController {
+        let view = view
+        let navController = UINavigationController(rootViewController: view)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)?.withRenderingMode(.automatic)
+        tabList.append(navController)
+        return navController
+    }
+
+}
