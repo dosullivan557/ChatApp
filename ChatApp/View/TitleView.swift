e8c764be604a54bf1906a3bf63b0611d7a02a481
Dan <dosullivan557@gmail.com>
2018-01-13 20:41:29 +0000

    Removed titleview

---
+++ ChatApp/View/TitleView.swift
@@ -0,0 +1,21 @@
+//
+//  TitleView.swift
+//  ChatApp
+//
+//  Created by Danny on 13/01/2018.
+//  Copyright © 2018 Danny. All rights reserved.
+//
+
+import UIKit
+
+class TitleView: UIBarButtonItem {
+    let profilepicture: UIImageView = {
+        let pp = UIImageView()
+        
+        return pp
+    }()
+    override func viewDidLoad() {
+        
+    }
+    
+}
