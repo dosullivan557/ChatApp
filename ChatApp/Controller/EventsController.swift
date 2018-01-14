549ae8927fa5d352c0ff32e94d7412c7f1775baa
Dan <dosullivan557@gmail.com>
2018-01-14 21:01:16 +0000

    Gets the events from the database

--- ChatApp/Controller/EventsController.swift
+++ ChatApp/Controller/EventsController.swift
@@ -7,89 +7,57 @@
 //
 
 import UIKit
-
+import Firebase
 class EventsController: UITableViewController {
+    var events = [Event]()
+    var timer: Timer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        observeUserEvents()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func observeUserEvents() {
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let ref = Database.database().reference().child("user-events").child(uid)
+        ref.observe(.childAdded, with: { (DataSnapshot) in
+            let newRef = ref.child(DataSnapshot.key)
+            newRef.observe(.childAdded, with: { (snapshot) in
+                let eventId = snapshot.key
+                let messagesReference = Database.database().reference().child("events").child(eventId)
+                
+                messagesReference.observeSingleEvent(of: .value, with: { (DataSnapshot) in
+                    print(DataSnapshot)
+                    if let dictionary = DataSnapshot.value as? [String: AnyObject] {
+                        let event = Event()
+                        event.title = dictionary["Title"] as? String
+                        event.desc = dictionary["Description"] as? String
+                        event.host = dictionary["Invitee"] as? String
+                        event.invitee = dictionary["Host"] as? String
+                        event.time = dictionary["Time"] as? Int
+                        self.events.append(event)
+                        print(dictionary)
+                        //cancelled timer, so only 1 timer gets called, and therefore the only reloads the table once
+                        self.timer?.invalidate()
+                        self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.handleReload), userInfo: nil, repeats: false)
+                    }
+                    
+                }, withCancel: nil)
+            })
+            
+        }, withCancel: nil)
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    //Reloads the table view
+    @objc func handleReload() {
+        DispatchQueue.main.async(execute: {
+            self.tableView.reloadData()
+        })
     }
+    
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
