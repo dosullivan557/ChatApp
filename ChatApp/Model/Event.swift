3c73454d7d0de308368884b6dfa9ba56f392c401
Dan <dosullivan557@gmail.com>
2018-01-14 20:31:43 +0000

    Created an event object

---
+++ ChatApp/Model/Event.swift
@@ -0,0 +1,18 @@
+//
+//  Event.swift
+//  ChatApp
+//
+//  Created by Danny on 14/01/2018.
+//  Copyright © 2018 Danny. All rights reserved.
+//
+
+import UIKit
+//Event object
+class Event: NSObject {
+    var title: String?
+    var desc: String?
+    var time: Int?
+    var host: String?
+    var invitee: String?
+}
+
