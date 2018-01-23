395ad6bb81adffb5dc2dfc6d2b82afc929de2734
Dan <dosullivan557@gmail.com>
2018-01-23 00:57:28 +0000

    Sorted out the tab so that I can pass user info to all tabs at the same time

--- ChatApp/View/MyTabBar.swift
+++ ChatApp/View/MyTabBar.swift
@@ -6,16 +6,47 @@
 //  Copyright © 2018 Danny. All rights reserved.
 //
 import UIKit
-
+import Firebase
 class MyTabBar: UITabBarController {
     var tabList = [UIViewController]()
+    let messagesController = MessagesController()
+    let eventController = EventsController()
+    let myProfileController = MyProfileController()
+    var currentUser = User()
     override func viewDidLoad() {
-        
-        createElements(view: MessagesController(), title: "Messages", imageName: "Messages")
-        createElements(view: EventsController(), title: "Events", imageName: "event")
-        createElements(view: ProfileController(), title: "Profile", imageName: "profile")
+        getCurrentUser()
 
+        createElements(view: messagesController, title: "Messages", imageName: "Messages")
+        createElements(view: eventController, title: "Events", imageName: "event")
+        createElements(view: myProfileController, title: "Profile", imageName: "profile")
+        
         viewControllers = tabList
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        let delegate = UIApplication.shared.delegate as? AppDelegate
+        if (currentUser.id != delegate?.currentUser.id) {
+            currentUser = (delegate?.currentUser)!
+            passUsersThrough(user: currentUser)
+        }
+    }
+    func getCurrentUser(){
+        Database.database().reference().child("users").child((Auth.auth().currentUser?.uid)!).observe(.value, with: { (DataSnapshot) in
+            if let dictionary = DataSnapshot.value as? [String: AnyObject]{
+                let user = User()
+                user.name = dictionary["name"] as? String
+                user.email = dictionary["email"] as? String
+                user.profileImageUrl = dictionary["profileImageUrl"] as? String
+                user.id = DataSnapshot.key
+                self.passUsersThrough(user: user)
+            }
+        }, withCancel: nil)
+    }
+    
+    func passUsersThrough(user: User) {
+        self.currentUser = user
+
+        self.eventController.currentUser = user
+        self.myProfileController.user = user
     }
     func createElements(view: UIViewController,title: String, imageName: String) {
         let view = view
