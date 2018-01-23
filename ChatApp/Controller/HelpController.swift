4e5d31cf34deab392ee0e26ce2e42f61f6afcf4f
Dan <dosullivan557@gmail.com>
2018-01-23 23:47:01 +0000

    Created a view for help

---
+++ ChatApp/Controller/HelpController.swift
@@ -0,0 +1,24 @@
+//
+//  HelpController.swift
+//  ChatApp
+//
+//  Created by Danny on 23/01/2018.
+//  Copyright © 2018 Danny. All rights reserved.
+//
+
+import UIKit
+
+class HelpController: UIViewController, UIWebViewDelegate {
+    
+    override func viewDidLoad() {
+//        self.hidesBottomBarWhenPushed = true
+//        super.viewDidLoad()
+//        let webV:UIWebView = UIWebView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+//        let url = URL(fileURLWithPath: "https://www.youtube.com/embed/9bZkp7q19f0")
+//        webV.loadRequest(url)
+//        webV.delegate = self as UIWebViewDelegate
+//        self.view.addSubview(webV)
+    }
+
+}
+
