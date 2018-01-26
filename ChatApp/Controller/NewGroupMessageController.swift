6103794b8c25ba4980bdf90d770e4a3adad39179
Dan <dosullivan557@gmail.com>
2018-01-26 17:40:18 +0000

    Defined a group messaging create page

--- ChatApp/Controller/NewGroupMessageController.swift
+++ ChatApp/Controller/NewGroupMessageController.swift
@@ -1,60 +1,57 @@
 //
-//  NewGroupMessageController.swift
+//  NewMessageController.swift
 //  ChatApp
 //
-//  Created by Danny on 23/01/2018.
-//  Copyright © 2018 Danny. All rights reserved.
+//  Created by Danny on 28/12/2017.
+//  Copyright © 2017 Danny. All rights reserved.
 //
-
 import UIKit
 import Firebase
-
 class NewGroupMessageController: UITableViewController {
-    let chatWithUsers = [User]()
+    //Constants.
     let cellId = "cellId"
-    var users = [User]()
+    //Variables
+    var AllUsers = [User]() {
+        didSet{
+            handleReload()
+        }
+    }
+    var selectedUsers = [User]()
     var timer: Timer?
     var messagesController = MessagesController()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UserCell.self, forCellReuseIdentifier: cellId)
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Group", style: .plain,target: self, action: #selector(handleGroup))
-        fetchUser()
+        navigationItem.rightBarButtonItem = nil
+//        fetchUser()
         navigationItem.leftBarButtonItem = UIBarButtonItem(title:"Cancel", style: .plain, target: self, action: #selector(handleCancel))
+        
     }
-    
-    @objc func handleGroup() {
-        print("Group")
+//    func addGroupToDataBase() {
+//        let ref = Database.database().reference().child("Groups").childByAutoId()
+//        ref.updateChildValues("Group Name": "GroupName", "People" : )
+//    }
+
+
+    @objc func handleCreateGroup() {
+        
+        
+            dismiss(animated: true)
+
+            
+            self.messagesController.showChatControllerForUser(selectedUsers)
+        
     }
-    
     @objc func handleCancel(){
         dismiss(animated: true, completion: nil)
     }
     //Defines the number of cells in the tableview.
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
-    }
-    //Fetches each user from the database, and populates the tableview with each user.
-    func fetchUser(){
-        Database.database().reference().child("users").observe(.childAdded, with: { (DataSnapshot) in
-            if let dictionary = DataSnapshot.value as? [String: AnyObject]{
-                let user = User()
-                user.name = dictionary["name"] as? String
-                user.email = dictionary["email"] as? String
-                user.profileImageUrl = dictionary["profileImageUrl"] as? String
-                user.id = DataSnapshot.key
-                self.users.append(user)
-                
-                self.users.sort(by: { (u1, u2) -> Bool in
-                    return u1.name! < u2.name!
-                })
-                self.timer?.invalidate()
-                self.timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(self.handleReload), userInfo: nil, repeats: false)
-            }
-        }, withCancel: nil)
         
+        return AllUsers.count
     }
+
     //Reloads the tableview.
     @objc func handleReload() {
         DispatchQueue.main.async(execute: {
@@ -62,37 +59,19 @@ class NewGroupMessageController: UITableViewController {
         })
     }
     //Each cell for the tableview.
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-        
-        // Configure the cell...
-        cell.textLabel!.text = "row: \(indexPath.row)"
-        
-        if cell.isSelected
-        {
-            cell.accessoryType = UITableViewCellAccessoryType.checkmark
-        }
-        else
-        {
-            cell.accessoryType = UITableViewCellAccessoryType.none
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId) as? UserCell
+        let user = AllUsers[indexPath.row]
+        cell?.profileImageView.image = UIImage(named: "defaultPic")
+        cell?.textLabel?.text = user.name
+        cell?.detailTextLabel?.text = user.email
+        if let profileImageUrl = user.profileImageUrl {
+            cell?.profileImageView.loadImageUsingCache(urlString: profileImageUrl)
+            
         }
         
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        return cell!
         
-        let cell = tableView.cellForRow(at: indexPath)
-        
-        if cell!.isSelected == true
-        {
-            cell!.accessoryType = UITableViewCellAccessoryType.checkmark
-        }
-        else
-        {
-            cell!.accessoryType = UITableViewCellAccessoryType.none
-        }
     }
 
     //Defines the height of each cell.
@@ -100,4 +79,35 @@ class NewGroupMessageController: UITableViewController {
         return 65
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        if !(selectedUsers.contains(AllUsers[indexPath.row]))
+        {
+            cell!.accessoryType = UITableViewCellAccessoryType.checkmark
+            selectedUsers.append(AllUsers[indexPath.row])
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title:"Create Group", style: .plain, target: self, action: #selector(handleCreateGroup))
+
+        }
+        else
+        {
+            cell!.accessoryType = UITableViewCellAccessoryType.none
+            print(indexPath.row)
+            if(selectedUsers.contains(AllUsers[indexPath.row])){
+            for i in 0...selectedUsers.count{
+                    if selectedUsers[i] == AllUsers[indexPath.row] {
+                        selectedUsers.remove(at: i)
+                        if selectedUsers.count == 0 {
+                            navigationItem.rightBarButtonItem = nil
+                        }
+                        break
+                    }
+                }
+            }
+
+            print(selectedUsers.count)
+        }
+    }
 }
+
