42eea8be06b45809ccd0ae628ce80882213cd244
Dan <dosullivan557@gmail.com>
2018-01-31 15:41:47 +0000

    Bug Fix

--- ChatApp/Controller/MessagesController.swift
+++ ChatApp/Controller/MessagesController.swift
@@ -162,11 +162,13 @@ class MessagesController: UITableViewController {
     }
     //defines how to open a new message controller when given an input.
     @objc func handleNewMessage() {
+        
         let newMessageController = NewMessageController()
         newMessageController.messagesController = self
         let navController = UINavigationController(rootViewController: newMessageController)
         present(navController, animated: true, completion: nil)
     }
+    
     //checks whether the user is logged in; if so, then fill in the information of the view, otherwise logout. This function is called when the app is first loaded.
     func checkIfUserIsLoggedIn() {
         if Auth.auth().currentUser?.uid == nil {
@@ -177,6 +179,7 @@ class MessagesController: UITableViewController {
     }
     //called when a tablecell is selected.
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
         let message = messages[indexPath.row]
         guard let chatId = message.chatWithId() else {
             return
