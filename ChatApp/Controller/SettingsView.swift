d65ebdd9f521bd3b7e3e9cdad70150072c0ad704
Dan <dosullivan557@gmail.com>
2018-02-19 22:27:29 +0000

    Added Settings view

---
+++ ChatApp/Controller/SettingsView.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsView.swift
+//  ChatApp
+//
+//  Created by Danny on 18/02/2018.
+//  Copyright © 2018 Danny. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SettingsView: UITableViewController {
+}
