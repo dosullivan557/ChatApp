dc3b224fdc2cf7b9561298583e85de50058d31cb
Dan <dosullivan557@gmail.com>
2018-02-19 22:28:48 +0000

    Added tableview functions

--- ChatApp/Controller/SettingsView.swift
+++ ChatApp/Controller/SettingsView.swift
@@ -10,6 +10,12 @@ import UIKit
 import Firebase
 
 class SettingsView: UITableViewController {
+    //constant variables.
+    let settings = ["Greeting Message", "Colours"]
+    var currentUser = User()
+    
+    
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -21,4 +27,19 @@ class SettingsView: UITableViewController {
         titleView.backgroundColor? = UIColor.clear
         navigationItem.titleView = titleView
     }
+    ///Gives the number of rows in the table view.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    }
+    ///Specifies each element in the table view.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+  
 }
