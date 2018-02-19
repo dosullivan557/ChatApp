8598fb225e3482192fe885a26f87e650eda19d73
Dan <dosullivan557@gmail.com>
2018-02-19 22:29:07 +0000

    Filled in information in settings view

--- ChatApp/Controller/SettingsView.swift
+++ ChatApp/Controller/SettingsView.swift
@@ -12,6 +12,7 @@ import Firebase
 class SettingsView: UITableViewController {
     //constant variables.
     let settings = ["Greeting Message", "Colours"]
+    let cellId = "cellId"
     var currentUser = User()
     
     
@@ -27,19 +28,41 @@ class SettingsView: UITableViewController {
         titleView.backgroundColor? = UIColor.clear
         navigationItem.titleView = titleView
     }
+    ///Reloads the table view using the main thread.
+    @objc func handleReload() {
+        DispatchQueue.main.async(execute: {
+            self.tableView.reloadData()
+        })
+    }
+    
     ///Gives the number of rows in the table view.
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settings.count
     }
     ///Specifies each element in the table view.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.textLabel!.text = settings[indexPath.row]
+        if indexPath.row == 0 {
+            cell.detailTextLabel?.text = currentUser.settings?.greeting!
+        }
         return cell
     }
+    ///Defines the height of each table cell.
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 60
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            let GV = UpdateGreeting()
+            GV.settings = currentUser.settings
+            show(GV, sender: self)
+        }
+        else {
+            print("2")
+        }
     }
   
 }
