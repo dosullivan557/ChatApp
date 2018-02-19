12c36b14d7801492a2f2f5cd84f4ca4db5fe9ac6
Dan <dosullivan557@gmail.com>
2018-02-19 22:27:55 +0000

    Set the title and nav bar items

--- ChatApp/Controller/SettingsView.swift
+++ ChatApp/Controller/SettingsView.swift
@@ -10,4 +10,15 @@ import UIKit
 import Firebase
 
 class SettingsView: UITableViewController {
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        tableView.register(UserCell.self, forCellReuseIdentifier: cellId)
+        let titleView = UITextView()
+        titleView.text = "Settings"
+        titleView.isEditable = false
+        titleView.isUserInteractionEnabled = false
+        titleView.backgroundColor? = UIColor.clear
+        navigationItem.titleView = titleView
+    }
 }
