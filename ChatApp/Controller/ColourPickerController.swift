7133b1d70673c11508f91e480479f5ba12853b95
Dan <dosullivan557@gmail.com>
2018-02-21 00:29:31 +0000

    Added Color Picker controller

---
+++ ChatApp/Controller/ColourPickerController.swift
@@ -0,0 +1,13 @@
+//
+//  ColourPickerController.swift
+//  ChatApp
+//
+//  Created by Danny on 20/02/2018.
+//  Copyright © 2018 Danny. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ColourPickerController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
+}
