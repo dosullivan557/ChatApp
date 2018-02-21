baab3cc2fe2332867c99b8822d4cce768197ebb3
Dan <dosullivan557@gmail.com>
2018-02-21 00:53:18 +0000

    Added Comments

--- ChatApp/Model/Settings.swift
+++ ChatApp/Model/Settings.swift
@@ -8,13 +8,18 @@
 
 import Foundation
 import UIKit
-
+///Settings object for users
 class Settings: NSObject {
+    
     var id : String?
     var greeting : String?
     var myColor : String?
     var theirColor: String?
     
+    /**
+     Gets the string of the event including all of its information. Used for development.
+         - Returns: The string containing all of the information.
+     */
     func toString() -> String {
         return ("id: \(id!), greeting: \(greeting!), myColor: \(myColor!), theirColor: \(theirColor!)")
     }
