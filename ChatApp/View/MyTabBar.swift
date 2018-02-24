ecbb7b7e041c6bffabba03c9f78e8f9fd0c367a6
Dan <dosullivan557@gmail.com>
2018-02-24 16:19:07 +0000

    Added comments to variables

--- ChatApp/View/MyTabBar.swift
+++ ChatApp/View/MyTabBar.swift
@@ -10,29 +10,33 @@ import Firebase
 
 /// My TabBar for MessagesController
 class MyTabBar: UITabBarController {
+    ///List of elements in the tab list.
     var tabList = [UIViewController]()
     
+    ///Checks if user is logged in, then passes the current user to here, so that I can pass it to all of the tabs.
     var currentUser = User() {
         didSet {
             self.passUsersThrough(user: self.currentUser)
         }
     }
-    
+    ///An instance of MessagesController.
     let messagesController = MessagesController()
+    
+    //An instance of EventsController.
     let eventController = EventsController()
+    
+    ///An instance of MyProfileController.
     let myProfileController = MyProfileController()
     
     override func viewDidLoad() {
-        
-        
-        
+        super.viewDidLoad()
         createElements(view: messagesController, title: "Messages", imageName: "Messages")
         createElements(view: eventController, title: "Events", imageName: "CalendarIcon")
         createElements(view: myProfileController, title: "Profile", imageName: "profile")
         
         viewControllers = tabList
-        
     }
+    
     override func viewDidAppear(_ animated: Bool) {
         let delegate = UIApplication.shared.delegate as? AppDelegate
         if (currentUser.id != delegate?.currentUser.id) {
@@ -40,9 +44,7 @@ class MyTabBar: UITabBarController {
             passUsersThrough(user: currentUser)
         }
     }
-    /**
-     * Gets the current users information.
-     */
+    ///Gets the current users information.
     func getCurrentUser(){
         Database.database().reference().child("users").child((Auth.auth().currentUser?.uid)!).observe(.value, with: { (DataSnapshot) in
             if let dictionary = DataSnapshot.value as? [String: AnyObject]{
@@ -69,7 +71,7 @@ class MyTabBar: UITabBarController {
     /**
      Passes the current user into the tabs so it can set them up.
      - Parameters:
-     - user: The current user to be passed to its views so they can be setup.
+         - user: The current user to be passed to its views so they can be setup.
      */
     func passUsersThrough(user: User) {
         self.messagesController.user = user
@@ -80,9 +82,9 @@ class MyTabBar: UITabBarController {
      Creates a tab element, and adds it to the current tabbar.
      
      - Parameters:
-     - view: The view to add.
-     - title: The title of the tab bar element to add.
-     - imageName: The name of the image to set as the tab bar item's image.
+         - view: The view to add.
+         - title: The title of the tab bar element to add.
+         - imageName: The name of the image to set as the tab bar item's image.
      */
     func createElements(view: UIViewController,title: String, imageName: String) {
         let view = view
