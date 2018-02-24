67664bce8a66a04758d59d2900ce23fd930973aa
Dan <dosullivan557@gmail.com>
2018-02-24 22:22:55 +0000

    Reformatted the code using marks

--- ChatApp/Controller/MyEventsController.swift
+++ ChatApp/Controller/MyEventsController.swift
@@ -12,8 +12,14 @@ import Firebase
 import EventKit
 import MapKit
 class MyEventsController: UITableViewController {
+    // MARK: - Constants
+    
+    
     ///The reuse cell identifier for the table view.
     let cellEId = "cellEId"
+    
+    // MARK: - Variables
+
     ///The list of events.
     var events = [Event]()
     ///Global timer to make ensure that the TableView is only refreshed once to prevent flickering when there are loads of cells to load.
@@ -28,6 +34,8 @@ class MyEventsController: UITableViewController {
         }
     }
     
+    //MARK: - View initialisation
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(EventCell.self, forCellReuseIdentifier: cellEId)
@@ -38,8 +46,68 @@ class MyEventsController: UITableViewController {
         setupNavBarWithUser(currentUser)
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if  currentUser.id == Auth.auth().currentUser?.uid {
+            return
+        }
+        else {
+            events.removeAll()
+            setupNavBarWithUser(currentUser)
+            observeUserEvents()
+            handleReload()
+        }
+    }
+    
+    //MARK: - Setup
+    
+    /**
+     Gets passed the current user of the system, and then sets up the navigation bar with that users information.
+     - Parameters:
+     - user: The current user of the application.
+     */
+    func setupNavBarWithUser(_ user: User?) {
+        tableView.reloadData()
+        
+        self.navigationItem.title = ("My Events")
+        
+    }
+    
+    
+    
+    //MARK: - TableView
+    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 72
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    /// Reloads the table view.
+    @objc func handleReload(){
+        DispatchQueue.main.async() {
+            self.tableView.reloadData()
+        }
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellEId", for: indexPath) as! EventCell
+        cell.event = events[indexPath.row]
+        cell.textLabel?.text = events[indexPath.row].title
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let event = events[indexPath.row]
+        let eventController = EventController()
+        eventController.event = event
+        show(eventController, sender: self)
     }
     
     
@@ -65,116 +133,7 @@ class MyEventsController: UITableViewController {
         }
     }
     
-    /**
-     Uploads any errors to the database for examination.
-     - Parameters:
-         - error: The error code which is called.
-     */
-    func postError(error: Error){
-        let ref = Database.database().reference().child("Error").child(NSUUID().uuidString)
-        let values = ["Error Description": error.localizedDescription]
-        ref.updateChildValues(values as [String: AnyObject])
-    }
-    
-    /**
-     Shows alerts for the given message and title. Calls [createAlertButton]() to add in the relevant buttons onto the alert.
-     - Parameters:
-         - title: The title to set for the alert box.
-         - message: The message to set for the alert box.
-     
-     */
-    
-    func showAlert(title: String, message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { (x) in
-            alert.dismiss(animated: true, completion: nil)
-        }))
-        self.present(alert, animated: true, completion: nil)
-    }
-    /**
-     Gets passed the current user of the system, and then sets up the navigation bar with that users information.
-     - Parameters:
-         - user: The current user of the application.
-     */
-    func setupNavBarWithUser(_ user: User?) {
-        tableView.reloadData()
-        
-        self.navigationItem.title = ("My Events")
-        
-    }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return events.count
-    }
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    
-    override func viewDidAppear(_ animated: Bool) {
-        if  currentUser.id == Auth.auth().currentUser?.uid {
-            return
-        }
-        else {
-            events.removeAll()
-            setupNavBarWithUser(currentUser)
-            observeUserEvents()
-            handleReload()
-        }
-    }
-    
-    /// Reloads the table view.
-    @objc func handleReload(){
-        DispatchQueue.main.async() {
-            self.tableView.reloadData()
-        }
-    }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellEId", for: indexPath) as! EventCell
-        cell.event = events[indexPath.row]
-        cell.textLabel?.text = events[indexPath.row].title
-        return cell
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let event = events[indexPath.row]
-        let eventController = EventController()
-        eventController.event = event
-        show(eventController, sender: self)
-    }
-    
-    /**
-     If the event has been accepted by the other user, it adds the event to their calendar.
-     - Parameters:
-         - eventToAdd: The event to add to the calendar.
-     */
-    func addEventToCalendar(eventToAdd: Event) {
-        let eventStore: EKEventStore = EKEventStore()
-        eventStore.requestAccess(to: .event) { (bool, error) in
-            if (bool) && (error == nil) {
-                print("granted \(bool)")
-                let event:EKEvent = EKEvent(eventStore: eventStore)
-                event.title = eventToAdd.title
-                event.startDate = Date(timeIntervalSince1970: eventToAdd.startTime as! TimeInterval)
-                event.endDate = Date(timeIntervalSince1970: eventToAdd.finishTime as! TimeInterval)
-                event.notes = eventToAdd.desc
-                event.calendar = eventStore.defaultCalendarForNewEvents
-                do {
-                    try eventStore.save(event, span: .thisEvent)
-                } catch let error as NSError {
-                    self.showAlert(title: "Error", message: "We have run into an issue whilst creating the event. We have informed the developer to this issue")
-                    self.postError(error: error)
-                    return
-                }
-            }
-            else {
-                self.showAlert(title: "Error", message: "We have run into an issue whilst creating the event. We have informed the developer to this issue")
-                self.postError(error: error!)
-                return
-            }
-        }
-    }
+    //MARK: - Firebase
     
     ///Gets all the users events.
     func observeUserEvents() {
@@ -219,6 +178,72 @@ class MyEventsController: UITableViewController {
             })
         }, withCancel: nil)
     }
+    
+    
+    /**
+     Uploads any errors to the database for examination.
+     - Parameters:
+         - error: The error code which is called.
+     */
+    func postError(error: Error){
+        let ref = Database.database().reference().child("Error").child(NSUUID().uuidString)
+        let values = ["Error Description": error.localizedDescription]
+        ref.updateChildValues(values as [String: AnyObject])
+    }
+    
+    //MARK: - Alert
+    
+    /**
+     Shows alerts for the given message and title. Calls [createAlertButton]() to add in the relevant buttons onto the alert.
+     - Parameters:
+         - title: The title to set for the alert box.
+         - message: The message to set for the alert box.
+     
+     */
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { (x) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+  
+    //MARK: - Smart Features
+ 
+    /**
+     If the event has been accepted by the other user, it adds the event to their calendar.
+     - Parameters:
+         - eventToAdd: The event to add to the calendar.
+     */
+    func addEventToCalendar(eventToAdd: Event) {
+        let eventStore: EKEventStore = EKEventStore()
+        eventStore.requestAccess(to: .event) { (bool, error) in
+            if (bool) && (error == nil) {
+                print("granted \(bool)")
+                let event:EKEvent = EKEvent(eventStore: eventStore)
+                event.title = eventToAdd.title
+                event.startDate = Date(timeIntervalSince1970: eventToAdd.startTime as! TimeInterval)
+                event.endDate = Date(timeIntervalSince1970: eventToAdd.finishTime as! TimeInterval)
+                event.notes = eventToAdd.desc
+                event.calendar = eventStore.defaultCalendarForNewEvents
+                do {
+                    try eventStore.save(event, span: .thisEvent)
+                } catch let error as NSError {
+                    self.showAlert(title: "Error", message: "We have run into an issue whilst creating the event. We have informed the developer to this issue")
+                    self.postError(error: error)
+                    return
+                }
+            }
+            else {
+                self.showAlert(title: "Error", message: "We have run into an issue whilst creating the event. We have informed the developer to this issue")
+                self.postError(error: error!)
+                return
+            }
+        }
+    }
+    
+   
 }
 
 
