36ba4fff68f1154fa83d0b946fb51ce6ac6c40a1
Dan <dosullivan557@gmail.com>
2018-02-24 16:18:56 +0000

    Added comments to variables

--- ChatApp/Model/Settings.swift
+++ ChatApp/Model/Settings.swift
@@ -10,10 +10,13 @@ import Foundation
 import UIKit
 ///Settings object for users
 class Settings: NSObject {
-    
+    ///The id of the user these settings are for.
     var id : String?
+    ///The users personalised message for starting a conversation.
     var greeting : String?
+    ///The color the user wants to set for their side of the conversation.
     var myColor : String?
+    ///The color the user wants to set for the other user's side of the conversation.
     var theirColor: String?
     
     /**
