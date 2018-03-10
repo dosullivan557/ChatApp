78bc24d327b93185d39dccc38e4416107058e66d
Dan <dosullivan557@gmail.com>
2018-03-10 00:35:05 +0000

    Added StatusController

---
+++ ChatApp/Controller/StatusController.swift
@@ -0,0 +1,13 @@
+//
+//  StatusController.swift
+//  ChatApp
+//
+//  Created by Danny on 09/03/2018.
+//  Copyright © 2018 Danny. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class StatusController: UIViewController {
+}
