8bfcc25f70e0b7a43d22393e60eb922533d64a60
Dan <dosullivan557@gmail.com>
2018-03-11 19:08:25 +0000

    Created ActivityController

---
+++ ChatApp/Controller/ActivityController.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityController.swift
+//  ChatApp
+//
+//  Created by Danny on 11/03/2018.
+//  Copyright © 2018 Danny. All rights reserved.
+//
+
+import UIKit
+
+class ActivityController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.tag = 1
+    }
+    
+}
