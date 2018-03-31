34396077495907d09d5de68cc31df6ce7d56119d
Dan <dosullivan557@gmail.com>
2018-03-31 16:19:00 +0100

    Updated the tuple class to implement polymorphism

--- ChatApp/Model/Tuple.swift
+++ ChatApp/Model/Tuple.swift
@@ -7,24 +7,59 @@
 //
 
 import UIKit
-//user object
-class Tuple: NSObject {
-    var stringOne: String?
-    var stringTwo: String?
+/**
+ Tuple object using polymorphism and generics.
+     let tuple = Tuple<String, String>()
+ This allows the developer to set whatever data type they would like to use for both objects.
+ */
+class Tuple<A,B>: NSObject {
+    ///The first object.
+    var objectOne: A?
+    ///The second object.
+    var objectTwo: B?
     
-    func getStringOne() -> String{
-        return stringOne!
+    /**
+     Sets both of the variables in the tuple.
+     - Parameters:
+        - s1: First object to set.
+        - s2: Second object to set.
+     */
+    init(s1: A, s2: B) {
+        objectOne = s1
+        objectTwo = s2
     }
     
-    func getStringTwo() -> String{
-        return stringTwo!
+    /**
+     This returns the unwrapped element of the structure.
+     - Returns: The first element of the datastructure.
+     */
+    func getObjectOne() -> A{
+        return objectOne!
     }
     
-    func setStringOne(s: String){
-        stringOne = s
+    /**
+     This returns the unwrapped element of the structure.
+     - Returns: The Second element of the datastructure.
+     */
+    func getObjectTwo() -> B{
+        return objectTwo!
     }
     
-    func setStringTwo(s: String){
-        stringTwo = s
+    /**
+     This sets the first element in the datastructure. Used to overwrite the current values.
+     - Parameters:
+         - s: The object to set the object one to.
+     */
+    func setObjectOne(s: A){
+        objectOne = s
+    }
+    
+    /**
+     This sets the second element in the datastructure. Used to overwrite the current values.
+     - Parameters:
+         - s: The object to set the object two to.
+     */
+    func setObjectTwo(s: B){
+        objectTwo = s
     }
 }
